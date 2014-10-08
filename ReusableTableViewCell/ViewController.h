440965b826cf8e594a90cdbf7beb468c8c99d424
Guilherme Teodoro <gui.teo2@gmail.com>
2014-10-08 09:38:11 -0300

    Initial Commit

---
+++ ReusableTableViewCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReusableTableViewCell
+//
+//  Created by Guilherme Teodoro on 10/8/14.
+//  Copyright (c) 2014 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
